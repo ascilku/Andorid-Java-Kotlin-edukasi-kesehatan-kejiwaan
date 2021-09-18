6f4afd989c7332718b5dae292ab3bb5fb3c81f88
allamistiqha <alamistqiha@gmail.com>
2021-09-18 00:55:34 -0700

    Creating Project

---
+++ app/src/main/java/com/example/edukasikesehatankejiwaan/SplashScren.kt
@@ -0,0 +1,18 @@
+package com.example.edukasikesehatankejiwaan
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScren : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
+    }
+}
